eb120b3cf7e03b52a57ca52cb9598e0b101d09ed
Oscar <159105639+ofang11@users.noreply.github.com>
2025-08-02 16:53:28 -0400

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.py
@@ -0,0 +1,20 @@
+# Last updated: 8/2/2025, 4:53:28 PM
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+        res = -1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                res = mid
+                break
+        return res
